be8f2234c26be38966bea60ab3bf15f39cd69aed
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-27 21:28:10 +0200

    add function to change background of page with countown

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fineCD = new Date("04/28/2023 09:30:00").getTime();
-console.log(fineCD);
  setInterval(countDown, 1000);
 
 function countDown() {
@@ -10,5 +9,14 @@ function countDown() {
     const minuti = Math.floor((durataCD % (1000 * 60 * 60)) / (1000 * 60));
     const secondi = Math.floor((durataCD % (1000 * 60)) / 1000);
     
+    backgroud(ore, minuti);
+
     document.getElementById("timer").innerText = `${giorni}:${ore}:${minuti}:${secondi}`;
+}
+
+
+function backgroud(ore, minuti){
+    for (let i = 1; 13 > i; i++) {
+        document.getElementById(`colonna${i}`).style.backgroundColor = `rgb(${ore * i}, ${minuti * i}, ${i * 20})`;
+    }
 }
